4904cf2191cc532b46b7e607aaeed6597d040b57
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-16 14:21:34 +0000

    updated task 4

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""Initializes the views package for the v1 API."""
+"""Initialize Blueprint views"""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Avoid circular import errors
 from api.v1.views.index import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.places import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
-"""Defines the routes for the v1 API status."""
+"""index.py to connect to API"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, Blueprint, jsonify
+from models import storage
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def api_status():
-    """Returns the status of the API."""
+
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
+
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+
+if __name__ == "__main__":
+    pass
