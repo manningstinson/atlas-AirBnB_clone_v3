9f578b87055a08658a6772d29625fd7c0c940dcb
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-16 14:22:57 +0000

    updated task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""app.py to connect to API"""
+"""app.py: Module to configure and run the Flask application for the API"""
+
 import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
-
 @app.teardown_appcontext
-def teardown_appcontext(code):
-    """teardown_appcontext"""
+def teardown_appcontext(error):
+    """Teardown app context: Closes the database connection"""
     storage.close()
-
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """Error handler for 404 Not Found"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
