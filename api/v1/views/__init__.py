ec278375bd9794defe6c894b6fb7890e0268cd3b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-18 20:17:02 +0000

    updated task 9

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,4 +9,5 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Circular import is avoided by importing at the end
 from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.cities import *  # Add this line to import the cities view
+from api.v1.views.cities import *
+from api.v1.views.amenities import *  # Add this line to import the amenities view
