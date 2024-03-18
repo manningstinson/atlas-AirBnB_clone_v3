b72bfc3c3c4a3533e3b41235b5b0cb12d21f42c1
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-18 20:26:01 +0000

    updated task 12

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,4 +12,5 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
-from api.v1.views.places import *  # Add this line to import the places view
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *  # Add this line to import the places_reviews view
