ee2c130d15d0c14437e542c7399585242534a7db
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-18 20:04:33 +0000

    updated task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,22 @@ Module defining routes for version 1 of the API.
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Get status of the API."""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Get statistics of objects."""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
